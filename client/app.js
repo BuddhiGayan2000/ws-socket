d47bae9ef56d69ab1b7c4cc345bf431d20334e0b
BuddhiGayan2000 <108883603+BuddhiGayan2000@users.noreply.github.com>
2025-04-02 12:26:55 +0530

    Implement Socket.IO for WebSocket communication and update client interface

--- client/app.js
+++ client/app.js
@@ -1,10 +1,25 @@
-const socket = new WebSocket('ws://localhost:8080');
+//basic implementation for ws server, but I'm upgrading it using socket.io
 
-//listen for messages
-socket.onmessage = ({ data}) => {
-    console.log('Message from server', data);
-}
+// const socket = new WebSocket('ws://localhost:8080');
+
+// //listen for messages
+// socket.onmessage = ({ data}) => {
+//     console.log('Message from server', data);
+// }
+
+// document.querySelector('button').onclick = () => {
+//     socket.send('Hello');
+// }
+
+const socket = io('ws://localhost:8080');
+
+socket.on('message', text => {
+    const el = document.createElement('li');
+    el.innerHTML = text;
+    document.querySelector('ul').appendChild(el);
+})
 
 document.querySelector('button').onclick = () => {
-    socket.send('Hello');
+    const text = document.querySelector('input').value;
+    socket.emit('message', text);
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,34 @@
-const websocket = require('ws');
-const server = new websocket.Server({ port: '8080'});
 
-server.on('connection' , socket => {
-    socket.on('message', message => {
-        console.log('message is:', message);
-        socket.send(`Roger that! ${message}`);
-    });
+// this is a normal ws server lack of the capability to broadcasting. This just create a full-duplex connection between client and server.
+
+// const websocket = require('ws');
+// const server = new websocket.Server({ port: '8080'});
+
+// server.on('connection' , socket => {
+//     socket.on('message', message => {
+//         console.log('message is:', message);
+//         socket.send(`Roger that! ${message}`);
+//     });
+// });
+
+const http = require('http').createServer();
+
+const io = require('socket.io')(http, {
+    cors: { origin: '*' }
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('message', (message) => {
+        console.log(message);
+        // reemitting the message so it will be broadcasted to everybody listening
+        io.emit('message', `${socket.id.substr(0,2)} said ${message}`);
+    });
+    
+})
+
+http.listen(8080, () => {
+    console.log('listening on *:8080');
+})
+
